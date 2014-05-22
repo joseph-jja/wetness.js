25cc7157b0082bc5208ab370846ede8e524d11f2
Joe Acosta <joseph.acosta@macys.com>
2014-05-22 10:39:14 -0700

    update to parse css a bit better and show which rules are duping each other

--- wetness.js
+++ wetness.js
@@ -1,63 +1,78 @@
-var cssFile, fs = require( 'fs' ),
-    readline = require( 'readline' ),
+var cssFile, fs = require('fs'),
+    readline = require('readline'),
     properties = [],
     dupes = [],
     devNull,
     inside = false,
-    filedata, lines;
+    filedata, lines, rulename;
 
-cssFile = process.argv[ 2 ];
+cssFile = process.argv[2];
 
-if ( !cssFile ) {
-    console.log( "Usage: node wetness.js /path/to/file.css" );
+if (!cssFile) {
+    console.log("Usage: node wetness.js /path/to/file.css");
     return;
 }
 
-if ( !fs.statSync( cssFile ).isFile() ) {
-    console.log( "Error: Supplied CSS file does not exist." );
+if (!fs.statSync(cssFile).isFile()) {
+    console.log("Error: Supplied CSS file does not exist.");
     return;
 }
 
-function strip( str ) {
-    return str.replace( /^ */g, '' ).replace( / *$/g, '' ).replace( /;/, '' );
+function strip(str) {
+    return str.replace(/^ */g, '').replace(/ *$/g, '').replace(/;/, '');
 }
 
-filedata = fs.createReadStream( cssFile );
-devNull = fs.createWriteStream( '/dev/null' );
+filedata = fs.createReadStream(cssFile);
+devNull = fs.createWriteStream('/dev/null');
 
-lines = readline.createInterface( {
+lines = readline.createInterface({
     input: filedata,
     output: devNull
-} );
+});
 
-lines.on( 'line', function ( cmd ) {
-    var property, value, linex;
-    if ( cmd && cmd.match( /\{/ ) && !cmd.match( /\// ) ) {
+lines.on('line', function(cmd) {
+    var property, value, linex, o, rn;
+    if (cmd && cmd.match(/\{/) && !cmd.match(/\//)) {
         inside = true;
-    } else if ( cmd && cmd.match( /\}/ ) && !cmd.match( /\// ) ) {
+        rn = cmd.substring(0, cmd.indexOf("{"));
+        console.log('rn ' + rn);
+        if (rn.replace(/[\s|\t]*/g, '') !== '') {
+            rulename = rn;
+        }
+    } else if (cmd && cmd.match(/\}/) && !cmd.match(/\//)) {
         inside = false;
-    } else if ( inside && cmd.match( /\:/ ) ) {
+    } else if (inside && cmd.match(/\:/)) {
 
-        property = cmd.split( ":" )[ 0 ];
-        property = strip( property );
-        value = cmd.split( ":" )[ 1 ];
-        value = strip( value );
+        property = cmd.split(":")[0];
+        property = strip(property);
+        value = cmd.split(":")[1];
+        value = strip(value);
 
         linex = property + ":" + value + ";";
 
-        if ( properties[ linex ] ) {
-            properties[ linex ]++;
-            dupes[ linex ] = properties[ linex ];
-            console.log( dupes[ linex ] );
-        } else {
-            properties[ linex ] = 1;
-        }
-    }
-} );
+        o = {
+            'name': [rulename],
+            'value': linex
+        };
 
-lines.on( 'close', function ( cmd ) {
-    var i;
-    for ( i in dupes ) {
-        console.log( i + " " + dupes[ i ] );
+        if (properties[linex]) {
+            o = properties[linex];
+            o.count++;
+            o.name.push(rulename);
+            properties[linex] = o;
+            dupes[linex] = properties[linex];
+        } else {
+            o['count'] = 1;
+            properties[linex] = o;
+        }
+    } else if (!inside && cmd.replace(/[\s|\t]*/g, '') !== '') {
+        rulename = cmd;
     }
-} );
+});
+
+lines.on('close', function(cmd) {
+    var i;
+    for (i in dupes) {
+        console.log(i + " " + JSON.stringify(dupes[i]));
+    }
+});
